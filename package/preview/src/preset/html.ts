c8d3a6a37d20add9c0a80464a4acfcafa7e73ae8
Asnow <ganrn@vip.qq.com>
2022-10-29 23:14:33 +0800

    feat(*):移除默认的 html 预设, 并加入测试配置

--- package/preview/src/preset/html.ts
+++
@@ -1,9 +0,0 @@
-import type { BridgeData } from "../bridge/bridgeFile";
-
-export async function render(getMod: () => Promise<Object>, bridgeData: BridgeData) {
-    let iframe = document.createElement("iframe");
-    iframe.src = "/" + bridgeData.mapFileRelPath;
-    iframe.frameBorder = "0";
-    Object.assign(iframe.style, { width: "100%", height: `100%` });
-    document.body.append(iframe);
-}
